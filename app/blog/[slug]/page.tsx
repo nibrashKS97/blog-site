aa6e6a44fa90c613b9e8a0b2d3a19485d8a3dcfb
nibrashKS97 <kazisubahnibrash@gmail.com>
2024-05-30 19:59:31 +0600

    "Refactor BlogArticle component to fetch data from Sanity client and render title, image, and category"

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,13 +1,39 @@
-export default function BlogArticle({params}: {params:{slug:string}}) {
-    return (
-        <h1>{params.slug}</h1>
-    )
+import { client, urlFor } from "@/app/lib/sanity";
+import { fullBlog } from "@/lib/interface";
+import Image from "next/image";
 
+async function getData (slug: string) {
+    const query = `
+    *[_type == "blog" && slug.current == 'positive-work-culture']{
+        "currentSlug": slug.current,
+          title,
+          content,
+          titleImage,
+      }[0]`;
+
+      const data = await client.fetch(query);
+      return data;
 }
 
-*[_type == "blog" && slug.current == 'positive-work-culture']{
-    "currentSlug": slug.current,
-      title,
-      content,
-      titleImage,
-  }[0]
+export default async function BlogArticle({params}: {params:{slug:string}}) {
+    const data: fullBlog = await getData(params.slug);
+
+    console.log (data);
+
+    return (
+        <div className="mt-8">
+            <h1>
+                <span className="block text-base text-center text-primary font-bold tracking-wide uppercase">*Blog Category*</span>
+                <span className="block text-3xl text-center leading-8 font-bold tracking-tight sm:text-4xl">{data.title}</span>
+            </h1>
+            <Image 
+            src={urlFor(data.titleImage).url()}
+            width={800}
+            height={800}
+            alt="Title Image"
+            priority
+            className="rounded-lg mt-8 border"
+            />
+        </div>
+    )
+}

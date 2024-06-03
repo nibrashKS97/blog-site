5b663263e6153a71664bb428f893c55b6b01b3c4
nibrashKS97 <kazisubahnibrash@gmail.com>
2024-06-03 18:18:25 +0600

    "Updated blog page to use PortableText, removed interface file, and added typography plugin to tailwind config"

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,10 +1,11 @@
+import { fullBlog } from "@/app/lib/interface";
 import { client, urlFor } from "@/app/lib/sanity";
-import { fullBlog } from "@/lib/interface";
+import { PortableText } from "@portabletext/react";
 import Image from "next/image";
 
 async function getData (slug: string) {
     const query = `
-    *[_type == "blog" && slug.current == 'positive-work-culture']{
+    *[_type == "blog" && slug.current == '${slug}']{
         "currentSlug": slug.current,
           title,
           content,
@@ -18,8 +19,6 @@ async function getData (slug: string) {
 export default async function BlogArticle({params}: {params:{slug:string}}) {
     const data: fullBlog = await getData(params.slug);
 
-    console.log (data);
-
     return (
         <div className="mt-8">
             <h1>
@@ -27,13 +26,16 @@ export default async function BlogArticle({params}: {params:{slug:string}}) {
                 <span className="block text-3xl text-center leading-8 font-bold tracking-tight sm:text-4xl">{data.title}</span>
             </h1>
             <Image 
-            src={urlFor(data.titleImage).url()}
-            width={800}
-            height={800}
-            alt="Title Image"
-            priority
-            className="rounded-lg mt-8 border"
+                src={urlFor(data.titleImage).url()}
+                width={800}
+                height={800}
+                alt="Title Image"
+                priority
+                className="rounded-lg mt-8 border"
             />
+            <div className="mt-16 prose prose-blue prose-lg dark:prose-invert prose-li:marker:text-primary prose-a:text-primary">
+                <PortableText value={data.content} />
+            </div>
         </div>
     )
 }
